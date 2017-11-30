3bbefad025da5f17c87d429a92c45546d14265f6
bart27 <barna27.szab@gmail.com>
2017-12-01 00:33:25 +0100

    Add project files.

---
+++ Logger/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    class Logger
+    {
+        Action<string> log;
+
+        public void AddLogMethod(Action<string> logMethod)
+        {
+            log += logMethod;
+        }
+
+        public void Log(string message)
+        {
+            if (log != null)
+                log(DateTime.Now + " " + message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger log = new Logger();
+            log.AddLogMethod(x => Console.WriteLine(x));
+            log.AddLogMethod(x =>
+                {
+                    using (StreamWriter writer = new StreamWriter("log.txt"))
+                    {
+                        writer.WriteLine(x);
+                    }
+                });
+            log.Log("HIBA!!!!!!");
+            Console.ReadLine();
+        }
+    }
+}
